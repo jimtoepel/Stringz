3c18e8771eb72243192f7f49446cbab757041440
jimtoepel <jim.toepel@gmail.com>
2015-03-28 14:27:28 -0700

    error handling

--- Stringz/main.m
+++ Stringz/main.m
@@ -15,11 +15,25 @@ int main(int argc, const char * argv[]) {
         for (int i = 0; i < 10; i++) {
             [str appendString:@"Aaron is cool!\n"];
         }
-        [str writeToFile:@"/tmp/cool.txt"
-              atomically:YES
-                encoding:NSUTF8StringEncoding
-                   error:NULL];
-        NSLog(@"done writting /tmp/cool.txt");
+        
+        // Declare a pointer to an NSError object, but do not instantiate it.
+        // The NSError instance will only be created if there is, in fact, an error
+        
+        NSError *error;
+        
+        // Pass the NSError pointer by reference to the NSSTring Method
+        
+        BOOL success = [str writeToFile:@"/tmp/cool.txt"
+                             atomically:YES
+                               encoding:NSUTF8StringEncoding
+                                  error:&error];
+        
+        // Test the returned BOOl and query the NSError if the rite failed
+        if (success) {
+            NSLog(@"done writing /tmp/cool.txt");
+        } else {
+            NSLog(@"writing /tmp/cool.txt failed: %@", [error localizedDescription]);
+        }
         
     }
     return 0;
