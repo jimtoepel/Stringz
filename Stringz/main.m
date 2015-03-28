559fa473d89fd436b5fb5fb1ec9ce6097269ac5f
jimtoepel <jim.toepel@gmail.com>
2015-03-27 20:08:19 -0700

    initial commit

---
+++ Stringz/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Stringz
+//
+//  Created by Jim Toepel on 3/27/15.
+//  Copyright (c) 2015 FunderDevelopment. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
